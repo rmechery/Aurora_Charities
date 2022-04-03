8dc0702b4623ea9c65247e14e06a8c01ea94b431
groundhog3 <groundhog3@github.com>
2022-04-03 03:00:28 -0400

    the recycler view just sets the cardview xml file and the firebase file just tess database data fetching

---
+++ app/src/main/java/com/example/auroracharities/FirebaseTest.java
@@ -0,0 +1,44 @@
+package com.example.auroracharities;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Map;
+
+public class FirebaseTest {
+    private final FirebaseFirestore db;
+    private final String TAG = "FirebaseTest";
+
+    FirebaseTest(FirebaseFirestore db) {
+        this.db = db;
+    }
+
+    public void  doStuff(){
+        db.collection("Charities")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Map charityData = document.getData();
+                                Log.d(TAG, document.getId() + " => " + charityData);
+                                Log.v(TAG, (String)charityData.get("motto"));
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+}
