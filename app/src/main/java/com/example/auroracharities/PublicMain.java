f0b917c9de91783d7b32a7b5c257312e2c964baf
groundhog3 <groundhog3@github.com>
2022-04-08 12:06:44 -0400

    have to commit some of my changes back in

--- app/src/main/java/com/example/auroracharities/PublicMain.java
+++ app/src/main/java/com/example/auroracharities/PublicMain.java
@@ -2,6 +2,7 @@ package com.example.auroracharities;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,32 +10,26 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.auroracharities.data.model.Charities;
-import com.example.auroracharities.data.model.CharitiesAdapter;
-import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class PublicMain extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-
-    private ArrayList<Charities> charitiesList;
+    private RecyclerViewAdapter recyclerViewAdapter;
+    private List<Charities> charitiesList;
 
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
     private final String TAG = "PublicMain";
 
-    private CharitiesAdapter adapter;
-
     public void doStuff(){
-        //Log.v(TAG, charitiesList.toString());
-
         db.collection("Charities")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -43,10 +38,11 @@ public class PublicMain extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 Map charityData = document.getData();
-                                //Log.d(TAG, document.getId() + " => " + charityData);
-                                String motto = (String)(charityData.get("motto"));
-                                //charitiesList.add(new Charities("Title", motto, R.drawable.gfgimage));
-                                Log.d(TAG, charitiesList.toString());
+                                Log.d(TAG, document.getId() + " => " + charityData);
+                                Log.v(TAG, (String)charityData.get("motto"));
+                                String motto = (String)charityData.get("motto");
+                                charitiesList.add(new Charities("Title", motto, R.drawable.gfgimage));
+                                Log.v(TAG, charitiesList.toString());
                             }
                         } else {
                             Log.d(TAG, "Error getting documents: ", task.getException());
@@ -58,42 +54,37 @@ public class PublicMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        charitiesList = new ArrayList<Charities>();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_public_main);
-        charitiesList = new ArrayList<Charities>();
 
-        recyclerView = findViewById(R.id.recyclerView);
+        doStuff();
+        Log.v(TAG, charitiesList.toString());
+        //prepareCharities();
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        recyclerViewAdapter = new RecyclerViewAdapter(charitiesList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
 
 
-        Query query = FirebaseFirestore.getInstance()
-                .collection("Charities");
+        recyclerViewAdapter.setOnItemClickListener(new ClickListener<Charities>(){
+            @Override
+            public void onItemClick(Charities data) {
+                Toast.makeText(PublicMain.this, data.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
-        FirestoreRecyclerOptions<Charities> options = new FirestoreRecyclerOptions.Builder<Charities>()
-                .setQuery(query, Charities.class)
-                .build();
-
-        adapter = new CharitiesAdapter(options);
-        // Connecting Adapter class with the Recycler view*/
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
-
+        recyclerView.setAdapter(recyclerViewAdapter);
 
     }
 
-    // Function to tell the app to start getting
-    // data from database on starting of the activity
-    @Override protected void onStart()
-    {
-        super.onStart();
-        adapter.startListening();
-    }
-
-    // Function to tell the app to stop getting
-    // data from database on stopping of the activity
-    @Override protected void onStop()
-    {
-        super.onStop();
-        adapter.stopListening();
-    }
-
+//    private void prepareCharities(){
+//        charitiesList.add(new Charities("Charity 1", R.drawable.a4g_logo_background));
+//        charitiesList.add(new Charities("Charity 2", R.drawable.gfgimage));
+//        charitiesList.add(new Charities("Charity 3", R.drawable.gfgimage));
+//        charitiesList.add(new Charities("Charity 4", R.drawable.gfgimage));
+//        charitiesList.add(new Charities("Charity 5", R.drawable.gfgimage));
+//        charitiesList.add(new Charities("Charity 6", R.drawable.gfgimage));
+//        charitiesList.add(new Charities("Charity 7", R.drawable.gfgimage));
+//    }
 }
---
+++ app/src/main/java/com/example/auroracharities/RecyclerViewAdapter.java
@@ -0,0 +1,78 @@
+package com.example.auroracharities;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.auroracharities.data.model.Charities;
+
+import java.text.BreakIterator;
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>{
+
+    private List<Charities> charitiesList;
+    private ClickListener<Charities> clickListener;
+
+    RecyclerViewAdapter(List<Charities> charitiesList){
+        this.charitiesList = charitiesList;
+    }
+
+    @Override
+    public RecyclerViewAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_adapter_layout,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewAdapter.MyViewHolder holder, final int position) {
+
+        final Charities charity = charitiesList.get(position);
+
+        holder.title.setText(charity.getTitle());
+        holder.image.setBackgroundResource(charity.getImage());
+        holder.motto.setText(charity.getMotto());
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickListener.onItemClick(charity);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return charitiesList.size();
+    }
+
+    public void setOnItemClickListener(ClickListener<Charities> charityClickListener) {
+        this.clickListener = charityClickListener;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+
+        private TextView title;
+        private ImageView image;
+        private TextView motto;
+        private CardView cardView;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.title);
+            image = itemView.findViewById(R.id.image);
+            motto = itemView.findViewById(R.id.motto);
+            cardView = itemView.findViewById(R.id.carView);
+        }
+    }
+}
+
+interface ClickListener<T> {
+    void onItemClick(T data);
+}
