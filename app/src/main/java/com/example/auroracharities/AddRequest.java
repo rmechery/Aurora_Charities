07682846a679c8e368f2c15b2ba6af7a2833c310
groundhog3 <groundhog3@github.com>
2022-04-11 13:26:55 -0400

    we done fixed the app. On the way to finish the MVP.

--- app/src/main/java/com/example/auroracharities/AddRequest.java
+++ app/src/main/java/com/example/auroracharities/AddRequest.java
@@ -11,16 +11,30 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class AddRequest extends AppCompatActivity {
+public class AddRequest extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_request);
+
+        Spinner spinnerLanguages=findViewById(R.id.spinner_categories);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerLanguages.setAdapter(adapter);
+        spinnerLanguages.setOnItemSelectedListener(this);
     }
 
-    Spinner spinnerLanguages=findViewById(R.id.spinner_categories);
-    ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_item);
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
 
 
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
